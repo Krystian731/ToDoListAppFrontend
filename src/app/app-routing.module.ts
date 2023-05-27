54ea30bcf60ae6c0b5e2d6c79ead7a15505a136f
unknown <krystianolejniczak731@tlen.pl>
2023-05-27 14:02:52 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EditTaskComponent} from './edit-task/edit-task.component';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'AppComponent', pathMatch: 'full' },
+  { path: 'edit-card/:id', component:EditTaskComponent }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {FormsModule} from "@angular/forms";
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    EditTaskComponent
   ],
   imports: [
     BrowserModule,
